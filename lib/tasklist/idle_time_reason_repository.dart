d38cdee611c10a40ff4f6f94ea367aed274e797a
kostd <secret@nothing.com>
2022-02-16 22:01:20 +0300

    -- little fixes

--- lib/tasklist/idle_time_reason_repository.dart
+++ lib/tasklist/idle_time_reason_repository.dart
@@ -2,7 +2,7 @@ import 'package:get/get_core/src/get_main.dart';
 import 'package:get/get_instance/src/extension_instance.dart';
 import 'package:get/get_state_manager/src/rx_flutter/rx_disposable.dart';
 import 'package:tasklist_lite/state/application_state.dart';
-import 'package:tasklist_lite/tasklist/fixture/idle_time_reasom_fixtures.dart';
+import 'package:tasklist_lite/tasklist/fixture/idle_time_reason_fixtures.dart';
 
 class IdleTimeReasonRepository extends GetxService {
   List<String> getIdleTimeReasons() {
